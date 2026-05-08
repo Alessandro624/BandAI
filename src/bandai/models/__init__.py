509e8adc6ded94a58985380350ea4bceafd99393
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2026-05-08 11:45:33 +0200

    Add __init__.py inside models directory

---
+++ src/bandai/models/__init__.py
@@ -0,0 +1,3 @@
+from .models import RawContract, ResolvedContract, AdvocateAnalysis, AuditorChallenge, ComplianceVerdict, DepartmentBid, AuctionResult, FinalProposal
+
+__all__ = ["RawContract", "ResolvedContract", "AdvocateAnalysis", "AuditorChallenge", "ComplianceVerdict", "DepartmentBid", "AuctionResult", "FinalProposal"]
