1d1d03cca6b4b3ccc1366fde3af141d885fa6966
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2026-05-08 11:42:26 +0200

    Add __init__.py inside config directory

---
+++ src/bandai/config/__init__.py
@@ -0,0 +1,13 @@
+from .config import BANDI_PORTALS, COMPANY, PORTAL_WEIGHTS, _DEFAULT_PORTAL_WEIGHT, _MAX_REVIEW_ITERATIONS, IMPLICIT_NO_GO_KEYWORDS, CompanyProfile, get_llm, portal_weight
+
+__all__ = [
+    "BANDI_PORTALS",
+    "COMPANY",
+    "PORTAL_WEIGHTS",
+    "_DEFAULT_PORTAL_WEIGHT",
+    "_MAX_REVIEW_ITERATIONS",
+    "IMPLICIT_NO_GO_KEYWORDS",
+    "CompanyProfile",
+    "get_llm",
+    "portal_weight",
+]
